3e2b450502f0790dcc6f715b9e0e33407a7ed2a7
Shyok21 <shyokmutsuddi21@gmail.com>
2020-01-25 19:33:45 +0530

    gulpfile is now workable as beta version

---
+++ assets/gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minify = require('gulp-uglify');
+var concat = require('gulp-concat');
+watch = require('gulp-watch');
+gulp.task('hello',function(){
+   return console.log('GULP says Hi!');
+});
+gulp.task('copy-files',function(){
+    return gulp.src('src/html/*.html')
+    .pipe(gulp.dest('dest/html'));
+});
+gulp.task('sass',function(){
+    return gulp.src('src/css/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dest/css'));
+});
+gulp.task('minify-js',function(){
+    return gulp.src('src/javascript/*.js')
+    .pipe(minify())
+    .pipe(gulp.dest('dest/javascript'));
+});
+gulp.task('concat-js',function(){
+    return gulp.src('src/javascript/*.js')
+    .pipe(concat('app.js'))
+    .pipe(minify())
+    .pipe(gulp.dest('dest/javascript'));
+});
+gulp.task('concat-css',function(){
+    return gulp.src('src/css/*.scss')
+    .pipe(sass())
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('dest/css'));
+});
+gulp.task('default',function(){
+    gulp.watch('src/html/*.html',gulp.parallel('copy-files'));
+    gulp.watch('src/javascript/*.js',gulp.parallel('concat-js'));
+    gulp.watch('src/css/*.scss',gulp.parallel('concat-css'));
+});
+//gulp.task('default',['watch']);
