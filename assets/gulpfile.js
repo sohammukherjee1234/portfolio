45222398950deb82ce957637c48a7a42c7568063
Shyok21 <shyokmutsuddi21@gmail.com>
2020-01-25 19:44:26 +0530

    hot reloading packages installed but not available to use

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-sass');
 var minify = require('gulp-uglify');
 var concat = require('gulp-concat');
 watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
 gulp.task('hello',function(){
    return console.log('GULP says Hi!');
 });
@@ -32,9 +33,20 @@ gulp.task('concat-css',function(){
     .pipe(concat('style.css'))
     .pipe(gulp.dest('dest/css'));
 });
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        port: 8080,
+        open: true,
+        notify: false
+    });
+});
 gulp.task('default',function(){
     gulp.watch('src/html/*.html',gulp.parallel('copy-files'));
     gulp.watch('src/javascript/*.js',gulp.parallel('concat-js'));
     gulp.watch('src/css/*.scss',gulp.parallel('concat-css'));
+    //gulp.watch('dest',gulp.series('browser-sync'));
 });
 //gulp.task('default',['watch']);
